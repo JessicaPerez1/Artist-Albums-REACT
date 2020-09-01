3f14fc6730780277406b3b9bb31f78cfba9c1012
Jessica Perez <1jessicaperez@gmail.com>
2020-09-01 16:32:40 -0400

    Cards added

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -3,12 +3,37 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import AlbumCard from "./AlbumCard";
+import Col from "react-bootstrap/Col";
 
+import "./index.css";
 const Homepage = (props) => {
   return (
     <Container>
       <Row>
-        <AlbumCard />
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
+        <Col md={3}>
+          <AlbumCard />
+        </Col>
       </Row>
     </Container>
   );

50f4cb58d97ed5aa45b0ff21347e47e7a3296519
Jessica Perez <1jessicaperez@gmail.com>
2020-09-01 17:18:42 -0400

    Starting API

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,41 +1,34 @@
-import React from "react";
-//bootstrap component
+//React Components and Hooks
+import React, { useState, useEffect } from "react";
+//Bootstrap components
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import AlbumCard from "./AlbumCard";
 import Col from "react-bootstrap/Col";
-
+//Custom Components
+import AlbumCard from "./AlbumCard";
 import "./index.css";
-const Homepage = (props) => {
+
+const Homepage = () => {
+  const [albumState, setAlbumState] = useState([]);
+  useEffect(() => {
+    // For demonstration purposes, we mock an API call.
+    let albums = API.getAlbums.then((albums) => {
+      setAlbumState(albums);
+    });
+  }, []);
+
+  const dummyData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   return (
     <Container>
       <Row>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
-        <Col md={3}>
-          <AlbumCard />
-        </Col>
+        {dummyData.map((data) => (
+          <Col md={3}>
+            <AlbumCard />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
 };
+//mapping function that creates a row with every 3 cards
 export default Homepage;
